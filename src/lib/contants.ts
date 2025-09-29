ea4d6be5d10113e6cbf9d0e82c67370cad05d6ad
yeeway <yiwei.suuu@gmail.com>
2025-09-30 00:53:09 +0800

    feat: add SimpleMarquee component and shadcn config

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,3 +1,6 @@
+export const BEVY_RSVP_URL =
+  'https://gdg.community.dev/events/details/google-gdg-taipei-presents-devfest-taipei-2025/cohost-gdg-cloud-taipei'
+
 export const socialLinks = [
   { name: 'Facebook', href: 'https://facebook.com/gdg.taipei', icon: 'lineicons:facebook' },
   { name: 'Instagram', href: 'https://www.instagram.com/gdg.taipei', icon: 'lineicons:instagram' },
