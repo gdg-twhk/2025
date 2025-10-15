8bafcc38d87d8dbe25f13e07be18be8e6656a0f8
yeeway <yiwei.suuu@gmail.com>
2025-10-16 00:20:38 +0800

    refactor: remove output and trailingSlash options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
+  basePath: '/2025',
+  // output: 'export',
+  // trailingSlash: true,
 }
 
 export default nextConfig
