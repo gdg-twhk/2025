49f25be4a88d082f87b319b3cb336c4fec24e5c1
yeeway <yiwei.suuu@gmail.com>
2025-10-22 21:44:00 +0800

    feat: add open-graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,18 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: 'GDG DevFest Taipei 2025',
-    template: '%s | GDG DevFest Taipei 2025',
+    default: 'DevFest Taipei 2025',
+    template: '%s | DevFest Taipei 2025',
   },
   description:
     'DevFest Taipei 涵蓋廣泛 Mobile、Web、Cloud 及 AI，內容豐富多元。活動形式包括精彩的演講、實作工作坊、互動體驗等，並提供與 Google 工程師及其他開發者面對面交流的寶貴機會。作為開發者社群的重要活動，DevFest 為開發者提供了一個學習新知、拓展人脈，並共同成長的絕佳盛會。',
   openGraph: {
-    title: 'GDG DevFest Taipei 2025',
-    siteName: 'GDG DevFest Taipei 2025',
+    title: 'DevFest Taipei 2025',
+    siteName: 'DevFest Taipei 2025',
     description:
       'DevFest Taipei 涵蓋廣泛 Mobile、Web、Cloud 及 AI，內容豐富多元。活動形式包括精彩的演講、實作工作坊、互動體驗等，並提供與 Google 工程師及其他開發者面對面交流的寶貴機會。作為開發者社群的重要活動，DevFest 為開發者提供了一個學習新知、拓展人脈，並共同成長的絕佳盛會。',
-    url: 'https://2025.devfest-taipei.gdg.tw',
-    images: [{ url: 'https://2025.devfest-taipei.gdg.tw/og.png' }],
+    url: 'https://devfest-taipei.gdg.tw/2025/',
+    images: [{ url: 'https://devfest-taipei.gdg.tw/2025/og.png' }],
   },
 }
 
